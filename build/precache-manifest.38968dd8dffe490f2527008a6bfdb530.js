f819cfa70cfdf732aed7ac489033bc0ee9cb85d1
Joe Durrant <josephwdurrant@gmail.com>
2020-05-03 23:10:28 +0100

    Initial commit

---
+++ build/precache-manifest.38968dd8dffe490f2527008a6bfdb530.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efdfb4e57f72ec68c784807fe93ed62c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb77cd08aba5cd8f915d",
+    "url": "/static/js/2.8299f4b6.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/static/js/2.8299f4b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95b46b231ed4ae277aa2",
+    "url": "/static/js/main.57a31cc1.chunk.js"
+  },
+  {
+    "revision": "2fc89e7727ec7a823c45",
+    "url": "/static/js/runtime-main.ab31187a.js"
+  }
+]);
